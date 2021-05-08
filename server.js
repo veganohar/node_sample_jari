6a427da7a3bb4d8cab9cfcde48397dae609efd0b
Manohar Choudary <manoharchoudaryv@gmail.com>
2021-05-08 11:20:14 +0530

    all apis

--- server.js
+++ server.js
@@ -9,3 +9,19 @@ app.listen(port,()=>{
 app.get("/",(req,res)=>{
     res.send("Welcome to Node Js");
 })
+
+app.get("/getMethod",(req,res)=>{
+    res.send("Get Method is Working");
+})
+
+app.post("/postMethod",(req,res)=>{
+    res.send("Post Method is Working");
+})
+
+app.put("/putMethod",(req,res)=>{
+    res.send("Put Method is Working");
+})
+
+app.delete("/deleteMethod",(req,res)=>{
+    res.send("Delete Method is Working");
+})
